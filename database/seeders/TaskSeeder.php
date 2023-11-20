248e9655ab6b56ff9ca6b0efd9b9893f906455e0
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-11-20 14:51:36 +0800

    Added task seeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,16 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        if(! User::count()) {
+            User::factory(rand(5,10))->create();
+        }
+
+        User::query()
+            ->get()
+            ->each(
+                fn($user)
+                =>
+                Task::factory(rand(5,20))->create(['user_id' => $user->id])
+            );
     }
 }
