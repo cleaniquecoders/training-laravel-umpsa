a83dfb45033b3f39f69aff8dada7f876989db94e
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-11-21 10:48:08 +0800

    Update

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
                 {{ $tasks->links() }}
-                <table>
+                <table class="w-full">
                     <tr>
                         <th>Status</th>
                         <th>Task</th>
